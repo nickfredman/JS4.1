7fd062f3acac5cf99d4a02548caf76c389c5ac46
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-01-21 15:55:31 -0700

    Solutions to exercise #1

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).on('ready', function() {
-  
+  $('#button').click(function() {
+    $('body').append("Hello World");
+  });
+  $('#button2').click(function() {
+    $('body').append("<h1>Some text</h1>");
+  });
+  $('#button3').click(function() {
+    $('body').append("<ul><li></li><li></li><li></li></ul>");
+  });
 });
